7571cab7ea80514647b5603c93e25dc0b7281da1
Oscar <oscar5tob@gmail.com>
2026-07-28 12:56:11 -0400

    add value() to convert command values to int

--- test.py
+++ test.py
@@ -12,3 +12,7 @@ def conditional(operand_1: int, operator: str, operand_2: int) -> bool:
     else:
         return operand_1 < operand_2
 
+def value(x: int, data: dict) -> int:
+    if x in data:
+        return data[x]
+    return int(x)
