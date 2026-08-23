d0cb786b9372941c888d8244b0a00941980b1a88
Eduardo Boquillon <otorrealba019@gmail.com>
2026-08-23 01:46:18 -0500

    Fix: Remove excessive blank lines

--- interface.py
+++ interface.py
@@ -31,14 +31,7 @@ def lookup_cmd_file(existing_files: dict) -> str | None:
                     
     
 def command_ouput(cmd_result:list, selected_file: str) -> None:
+def command_ouput(cmd_result:list, selected_file: str) -> None:
     print("\n\n")
     print(f"Selected file: {selected_file}")
     print(f"Result: {cmd_result}")
-    
-    
-    
-
-
-        
-
-        
